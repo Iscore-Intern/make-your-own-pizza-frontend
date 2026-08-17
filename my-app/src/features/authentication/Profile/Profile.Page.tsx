5284adc99ce8b602c330167a72641d534b54736f
Kkarim-Ahmed <kkarim.1.aahmed@gmail.com>
2026-08-18 01:50:38 +0300

    feat: implement profile page UI and validation schema

--- my-app/src/features/authentication/Profile/Profile.Page.tsx
+++ my-app/src/features/authentication/Profile/Profile.Page.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ProfileHeader from "./ProfileHeader"
 import ProfileForm from "./ProfileForm";
 import { useProfileForm } from "./useProfileForm";
@@ -6,7 +7,10 @@ export default function ProfilePage() {
     const { formik, isEditing, onEdit, onCancel } = useProfileForm();
     return (
         <div className="page-container">
-            <ProfileHeader />
+            <ProfileHeader
+                firstName={formik.values.firstName}
+                lastName={formik.values.lastName}
+                email={formik.values.email} />
             <ProfileForm
                 formik={formik}
                 isEditing={isEditing}
