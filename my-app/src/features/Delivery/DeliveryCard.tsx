15bbb6df9934592c1f768076fbfe3a9a6fb1316c
Kkarim-Ahmed <kkarim.1.aahmed@gmail.com>
2026-09-02 15:48:14 +0300

    Refactor DeliveryCard component to use consistent parameter naming for deliveryDetails

--- my-app/src/features/Delivery/DeliveryCard.tsx
+++ my-app/src/features/Delivery/DeliveryCard.tsx
@@ -4,28 +4,28 @@ import DeliveryDetails from "@/Core/Interfaces/Delivery/DeliveryDetails.Interfac
 
 
 
-export default function DeliveryCard( DeliveryDetails : DeliveryDetails ) {
+export default function DeliveryCard( deliveryDetails : DeliveryDetails ) {
     const navigate=useNavigate();
     return (
         <div>
             <div className="flex justify-between items-center py-4 px-2">
                 {/* letf */}
                 <div className="flex flex-col gap-1">
-                    <p className="text-lg font-bold text-black-font">Order #{DeliveryDetails.OrderId}</p>
-                    <p className="text-base font-semibold text-black-font">{DeliveryDetails.CustomerName}</p>
-                    <p className="text-base font-semibold text-black-font">{DeliveryDetails.CustomerAddress}</p>
-                    <p className="text-base font-semibold text-black-font">{DeliveryDetails.CustomerPhone}</p>
-                    <p className="text-xs font-medium text-sub-color">{DeliveryDetails.OrderCreationTime}</p>
+                    <p className="text-lg font-bold text-black-font">Order #{deliveryDetails.OrderId}</p>
+                    <p className="text-base font-semibold text-black-font">{deliveryDetails.CustomerName}</p>
+                    <p className="text-base font-semibold text-black-font">{deliveryDetails.CustomerAddress}</p>
+                    <p className="text-base font-semibold text-black-font">{deliveryDetails.CustomerPhone}</p>
+                    <p className="text-xs font-medium text-sub-color">{deliveryDetails.OrderCreationTime}</p>
                 </div>
                 {/* middle */}
                 <div className="flex items-center flex-col gap-2">
-                    <p className="text-xl font-bold text-gray-800">{DeliveryDetails.PizzaCount}{DeliveryDetails.PizzaCount > 1 ? " Pizzas" : " Pizza"}</p>
-                    <p className="text-base font-bold text-gray-800">EGP {DeliveryDetails.TotalPrice}</p>
+                    <p className="text-xl font-bold text-gray-800">{deliveryDetails.PizzaCount}{deliveryDetails.PizzaCount > 1 ? " Pizzas" : " Pizza"}</p>
+                    <p className="text-base font-bold text-gray-800">EGP {deliveryDetails.TotalPrice}</p>
                 </div>
                 {/* right */}
                 <div className="flex items-center gap-3">
                     <button type="button"
-                    onClick={()=>{navigate(`/orders/${DeliveryDetails.OrderId}`)}}
+                    onClick={()=>{navigate(`/orders/${deliveryDetails.OrderId}`)}}
                     className="w-full px-4 py-3 rounded-xl bg-red-color text-white-color border-2 border-black-font border-r-4 border-b-4 hover:translate-x-0.5 hover:translate-y-0.5 hover:border-r-2 hover:border-b-2 transition-all duration-200">
                     View Order</button>
                 </div>
