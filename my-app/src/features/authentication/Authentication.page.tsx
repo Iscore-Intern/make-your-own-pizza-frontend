f9ac43de931f9a5eae4ddc8c3066d902fb642108
SamaSadek <Samaabdelsadek@gmail.com>
2026-09-16 13:18:37 +0300

    fix: ensure default landing page / always renders Login/SignUp page

--- my-app/src/features/authentication/Authentication.page.tsx
+++ my-app/src/features/authentication/Authentication.page.tsx
@@ -1,17 +1,7 @@
-import { Navigate } from "react-router-dom";
 import Authentication from "./authentication";
 import { useAuthPage } from "./useAuthPage";
-import { getAuthRole, getRoleDefaultPath } from "@/Core/Utils/auth.utils";
 
 export default function AuthenticationPage() {
     const authLogic = useAuthPage();
-
-    const token = localStorage.getItem("token");
-
-    if (token) {
-        const role = getAuthRole();
-        return <Navigate to={getRoleDefaultPath(role)} replace />;
-    }
-
     return <Authentication {...authLogic} />;
-}
+}
